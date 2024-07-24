b5ef3f1fd54c4926f8a13b88e5a992f610551cf9
Andrii <scoandrey@gmail.com>
2024-07-23 23:28:08 -0400

    second

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,15 +1,33 @@
+const uuid = require("uuid");
+const { Device } = require("../models/models");
+const apiError = require("../error/apiError");
 class DeviceController {
-    async create (req, res) {
+  async create(req, res, next) {
+try {
+    const { name, price, brandId, typeId, info } = req.body;
+    const { img } = req.files;
+    let fileName = uuid.v4() + ".jpg";
+    img.mv(path.resolve(__dirname, "..", "static", fileName));
 
-    }
+    const device = await Device.create({
+      name,
+      price,
+      brandId,
+      typeId,
+      img: fileName,
+    });
 
-    async getAll(req, res) {
-
-    }
-
-    async getOne(req, res) {
-        
-    }
+    return res.json(device);
+} catch (error) {
+    next(apiError.badRequest(error.message))
 }
 
-module.exports = new DeviceController()
+  
+  }
+
+  async getAll(req, res) {}
+
+  async getOne(req, res) {}
+}
+
+module.exports = new DeviceController();
--- server/index.js
+++ server/index.js
@@ -15,10 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
-app.use(fileUpload());
+app.use(fileUpload({}));
 app.use("/api", router);
-
-// Обработка ошибок
+npm 
 app.use(errorHandler);
 
 const start = async () => {
